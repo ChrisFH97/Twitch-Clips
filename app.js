22072e775568c6ecc901db6ea2d7b10315d07f6a
Chris Haig <CHAIG200@Caledonian.ac.uk>
2021-02-27 22:11:54 +0000

    Inital Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+app.use(express.static('public')) // relative path of client-side code
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
